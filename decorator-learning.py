af944e50e68a3341e8acf342d7b4ffa0ff0cae20
spiritedman4 <sivamsrini214@gmail.com>
2024-11-03 20:20:07 +0530

    first commit

---
+++ decorator-learning.py
@@ -0,0 +1,22 @@
+import time
+
+def calc_time(func):
+    def wrapper(*args,**kwargs):
+        start=time.time()
+        result=func(*args,**kwargs)
+        end=time.time()
+        print(f'The total time taken for {func.__name__} is {str((end-start)*10000)} ')
+        return result
+    return wrapper
+
+@calc_time
+def calc_square(numbers):
+    result=[]
+    for number in numbers:
+        result.append(number*number)
+    return result
+
+numbers=range(0,5)
+
+calc_square(numbers=numbers)
+
